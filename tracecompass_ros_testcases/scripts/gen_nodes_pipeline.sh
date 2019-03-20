73136ec0f23d26ee00b271cb4c02571ff5411385
Christophe Bedard <bedard.christophe@gmail.com>
2019-03-20 17:06:26 -0400

    Remove mention about number of messages for nodes_pipeline

--- tracecompass_ros_testcases/scripts/gen_nodes_pipeline.sh
+++ tracecompass_ros_testcases/scripts/gen_nodes_pipeline.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-## Generate a test trace for the nodes_pipeline test case with 1 message
-./${BASH_SOURCE%/*}/trace.sh nodes-pipeline-1 6 roslaunch tracecompass_ros_testcases nodes_pipeline.launch num_pubs:=1
+## Generate a test trace for the nodes_pipeline test case
+./${BASH_SOURCE%/*}/trace.sh nodes-pipeline 5 roslaunch tracecompass_ros_testcases nodes_pipeline.launch num_pubs:=1
